8bae81a0dd5236f4b4897bc46a747aeb2ce6c639
1009916248@qq.com <1009916248@qq.com>
2019-06-19 20:10:58 +0800

    二叉树删除最小值，最大值，遍历。

--- datastructure/src/main/java/com/algorithms/mc_ds/bst/BST.java
+++ datastructure/src/main/java/com/algorithms/mc_ds/bst/BST.java
@@ -1,5 +1,8 @@
 package com.algorithms.mc_ds.bst;
 
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class BST<E extends Comparable<E>> {
 
     private class Node<E>{
@@ -87,6 +90,71 @@ public class BST<E extends Comparable<E>> {
         return contains(root, e);
     }
 
+    /**
+     * 前序遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+    // 前序遍历以node为根的二分搜索树, 递归算法
+    private void preOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        System.out.print(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    /**
+     * 中序遍历
+     */
+    private void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.e);
+        inOrder(node.right);
+    }
+
+    /**
+     * 后序遍历
+     */
+    private void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.e);
+
+    }
+
+    //层序遍历
+    public void levelOrder() {
+        Queue<Node> queue = new LinkedBlockingQueue<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            Node cur = queue.remove();
+            System.out.println(cur.e);
+            if(cur.left != null) {
+                queue.add(cur.left);
+            }
+            if(cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
     //看以node为根的二分搜索树中是否包含元素e，递归算法。
     private boolean contains(Node node, E e) {
         if(node == null) {
@@ -97,5 +165,113 @@ public class BST<E extends Comparable<E>> {
         } else {
             return contains(node.right, e);
         }
+    }
+
+
+    public E miniE() {
+        if(root == null) {
+            return null;
+        }
+        return (E) miniNode(root).e;
+    }
+
+    public Node miniNode(Node node){
+        if(node.left == null) {
+            return node;
+        }
+        return miniNode(node.left);
+    }
+
+    public E removeMin() {
+        E minE = miniE();
+        root = removeMin(root);
+        return minE;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node) {
+        if(node.left == null) {
+            size--;
+            Node nodeRight = node.right;
+            return nodeRight;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    public E removeMax(){
+        if(root == null) {
+            return null;
+        }
+        E maxE = maxE();
+        root = removeMax(root);
+        return (E) root.e;
+    }
+
+    private E maxE() {
+        return (E)maxE(root).e;
+    }
+
+    private Node maxE(Node node) {
+        if(node == null) {
+            return null;
+        }
+        if(node.right == null) {
+            return node;
+        }
+        return maxE(node.right);
+    }
+
+    //移除以node 为根节点的二分搜索树中的最大节点
+    //返回删除节点后新的二分搜索树的根节点
+    private Node removeMax(Node node) {
+        if(node == null) {
+            return null;
+        }
+
+        if(node.right == null) {
+            //找到最大的node,
+            size--;
+            return node.left;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+//        int[] nums = {5, 3, 6, 8, 4, 2};
+        int[] nums = {5, 3, 4, 2};
+        for(int num: nums)
+            bst.add(num);
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+//        bst.preOrder();
+//        System.out.println();
+//        bst.inOrder();
+//        System.out.println();
+//        bst.postOrder();
+//        System.out.println();
+        bst.levelOrder();
+        System.out.println();
+        System.out.println(bst.maxE());
+        System.out.println();
+        bst.removeMax();
+//        bst.removeMax();
+//        bst.removeMax();
+        System.out.println(bst.maxE());
+        System.out.println();
+        bst.levelOrder();
+//        System.out.println();
+//        System.out.println(bst.miniE());
+//        System.out.println();
     }
 }
